e317053de4232407d97cfebf66bed19c19328dd9
Siddhesh Kankekar <kankekas@tcd.ie>
2021-10-21 12:28:27 +0100

    quick fix + update conditional session for miniProfile + Story

--- components/Stories.js
+++ components/Stories.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
 import faker from "faker";
 import Story from "./Story";
+import { useSession } from "next-auth/react";
 
 function Stories() {
   const [suggestions, setSuggestion] = useState([]);
+  const { data: session } = useSession();
 
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
@@ -19,6 +21,9 @@ function Stories() {
       className="flex space-x-2 p-6 bg-white mt-8 border-gray-200 border rounded-sm 
     overflow-x-scroll scrollbar-thin scrollbar-thumb-black"
     >
+      {session && (
+        <Story img={session.user.image} username={session.user.name} />
+      )}
       {suggestions.map((profile) => (
         <Story
           key={profile.id}
